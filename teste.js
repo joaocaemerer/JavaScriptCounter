df4acf229ad7eaa2c454b6c85ad293b4a96cb8cd
Fernando Caemerer <fernando.caemerer@gmail.com>
2022-05-24 15:54:40 -0300

    JavaScript Manual Counter

---
+++ teste.js
@@ -0,0 +1,19 @@
+// Comentario de linha "ctrl + /"
+
+/* 
+Comentario de multiplas linhas
+*/
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i=0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log('Da lista: ', array, '\nOs números pares são: ',evenNums);
+}
+
+let array = [1, 2, 4, 5, 6, 7, 8];
+
+returnEvenValues(array);
