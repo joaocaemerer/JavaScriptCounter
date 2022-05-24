d2a8e8e8435a518135690c4bc645c70b60e008e9
Fernando Caemerer <fernando.caemerer@gmail.com>
2022-05-24 16:02:07 -0300

    Using addEventListener for the buttons click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,23 @@
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
+var btnAdd = document.getElementsByName('btnAdd')[0];
+var btnSub = document.getElementsByName('btnSub')[0];
+
+btnAdd.addEventListener("click", function(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+});
+
+btnSub.addEventListener("click", function(){
+    if(currentNumber === 0){
+        return;
+    }else{
+        currentNumber = currentNumber - 1;
+    }   
+    currentNumberWrapper.innerHTML = currentNumber;
+});
+
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
